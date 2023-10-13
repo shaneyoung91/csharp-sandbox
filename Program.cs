72858a6ff3501eb53e8f5f7b7a6855ab68e0985c
shaneyoung91 <shaneadam.young@gmail.com>
2023-10-13 13:24:54 -0700

    Practice problems using if, else, else if statements

--- Program.cs
+++ Program.cs
@@ -25,46 +25,46 @@
     // The method should return a value of type int that represents the larger of the two arguments that you passed it.
 // You can assign the return value to largerValue on a new code line or on the existing code line that's used to declare largerValue.
 
-// int firstValue = 500;
-// int secondValue = 600;
-// int largerValue = Math.Max(firstValue, secondValue);
-// Console.WriteLine(largerValue);
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
 // --------------------------------------------
 
 
 // ------- Using if, else if, else, statements -------
-// Random dice = new Random();
+Random dice = new Random();
 
-// int roll1 = dice.Next(1, 7);
-// int roll2 = dice.Next(1, 7);
-// int roll3 = dice.Next(1, 7);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-// int total = roll1 + roll2 + roll3;
+int total = roll1 + roll2 + roll3;
 
-// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
-//     if ((roll1 == roll2) && (roll2 == roll3)) {
-//         Console.WriteLine("You rolled triples! +6 bonus to total!");
-//         total += 6;
-//     }
-//     else {
-//         Console.WriteLine("You rolled doubles! +2 bonus to total!");
-//         total += 2;
-//     }
-// }
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+    if ((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+}
 
-// if (total >= 16) {
-//     Console.WriteLine("You win a new car!");
-// }
-// else if (total >= 10) {
-//     Console.WriteLine("You win a new laptop!");
-// }
-// else if (total == 7) {
-//     Console.WriteLine("You win a trip for two!");
-// } else {
-//     Console.WriteLine("You win a kitten!");
-// }
+if (total >= 16) {
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10) {
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7) {
+    Console.WriteLine("You win a trip for two!");
+} else {
+    Console.WriteLine("You win a kitten!");
+}
 // ----------------------------------------------
 
 
@@ -129,6 +129,5 @@ if (daysUntilExpiration == 0) {
 if (discountPercentage > 0) {
     Console.WriteLine($"Renew now and save {discountPercentage}%!");
 }
-
 
 // -----------------------------------------
