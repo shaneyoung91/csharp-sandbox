f9ff6e0f4b3f48eb57ec71fec3403085308da451
shaneyoung91 <shaneadam.young@gmail.com>
2023-10-18 11:10:41 -0700

    switch-case construct exercise

--- Program.cs
+++ Program.cs
@@ -460,3 +460,169 @@ Console.WriteLine($"Total: {total}");
     Console.WriteLine($"Total: {total}");
 */
 
+
+/* -------- LESSON: BRANCHING LOGIC (SWITCH-CASE CONSTRUCT) ------------- */
+// A switch statement is C# selection statement that provides an alternative to an if-elseif-else branching construct.
+// The switch statement provides advantages over an if-elseif-else construct when evaluating a single value against a list of known matching values.
+// The switch statement chooses one section of code to execute from a list of possible switch sections.
+    // The selected switch sections is chosen based on a pattern match with the statement's match expression.
+
+/* Example
+    switch (fruit)
+    {
+        case "apple":
+            Console.WriteLine($"App will display information for apple.");
+            break;
+        
+        case "banana":
+            Console.WriteLine($"App will display information for bananas.");
+            break;
+
+        case "cherry":
+            Console.WriteLine($"App will display information for cherry.");
+            break;
+    }
+*/
+
+// Create and test a switch statement
+/*
+    int employeeLevel = 100;
+    string employeeName = "John Smith";
+
+    string title = "";
+
+    switch (employeeLevel)
+    {
+        case 100: // both case labels for 100 and 200 are now paired with the Senior Associate title
+        case 200:
+            title = "Senior Associate";
+            break;
+        case 300:
+            title = "Manager";
+            break;
+        case 400:
+            title = "Senior Manager";
+            break;
+        default:
+            title = "Associate";
+            break;
+    }
+
+    Console.WriteLine($"{employeeName}, {title}");
+*/
+
+// ------------------ CODE CHALLENGE -----------------------
+// Rewrite if-elseif-else using a switch statement
+
+/* ------ ORIGINAL CODE -----
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+if (product[0] == "01")
+{
+    type = "Sweat shirt";
+} else if (product[0] == "02")
+{
+    type = "T-Shirt";
+} else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}
+else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+} else if (product[1] == "MN")
+{
+    color = "Maroon";
+} else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+} else if (product[2] == "M")
+{
+    size = "Medium";
+} else if (product[2] == "L")
+{
+    size = "Large";
+} else
+{
+    size = "One Size Fits All";
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+*/
+
+// ------------- SOLUTION --------------
+/*
+    // SKU = Stock Keeping Unit. 
+    string sku = "01-MN-L";
+
+    string[] product = sku.Split('-');
+
+    string type = "";
+    string color = "";
+    string size = "";
+
+    switch (product[0])
+    {
+        case "01":
+            type = "Sweat shirt";
+            break;
+        case "02":
+            type = "T-Shirt";
+            break;
+        case "03":
+            type ="Sweat pants";
+            break;
+        default:
+            type = "Other";
+            break;
+    }
+
+    switch (product[1])
+    {
+        case "BL":
+            color = "Black";
+            break;
+        case "MN":
+            color = "Maroon";
+            break;
+        default:
+            color = "White";
+            break;
+    }
+
+    switch (product[2])
+    {
+        case "S":
+            size = "Small";
+            break;
+        case "M":
+            size = "Medium";
+            break;
+        case "L":
+            size = "Large";
+            break;
+        default:
+            size = "One Size Fits All";
+            break;
+    }
+
+    Console.WriteLine($"Product: {size} {color} {type}");
+*/
